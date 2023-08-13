595cf879c450b6d4af7898bdb4ef3681ce2e714f
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-08-13 15:19:07 +0900

    マウスによる螺旋の回転と移動

--- src/spiralSystem.ts
+++ src/spiralSystem.ts
@@ -1,10 +1,14 @@
 import { Vector3 } from "three"
-import { AUTO_GLOBAL_ROT_SPEED, ITEMS, SPIRAL_OFFSET_ANGLE_RAD, SPIRAL_OFFSET_Y } from "./define"
+import { AUTO_GLOBAL_ROT_SPEED, ITEMS, SPIRAL_LOOP, SPIRAL_OFFSET_ANGLE_RAD, SPIRAL_OFFSET_Y, SPIRAL_SPLIT } from "./define"
+import PointerState from "./PointerState"
 import renderingSystem from "./renderingSystem"
 import SpiralItem from "./SpiralItem"
 
 class SpiralSystem {
+  pointerState = new PointerState
   spiralRot = 0
+  spiralYByScroll = 0
+  spiralVelocity = { rot: 0, y: 0 }
   items!: SpiralItem[]
   init() {
     this.items = ITEMS.map((v, i) => {
@@ -12,27 +16,48 @@ class SpiralSystem {
     })
   }
 
-  calcItemPosition(i: number, spiralRot: number, position: Vector3) {
+  calcItemPosition(i: number, spiralRot: number, spiralY: number, position: Vector3) {
     const itemRot = SPIRAL_OFFSET_ANGLE_RAD * i + spiralRot
     const x = Math.sin(itemRot)
     const z = Math.cos(itemRot)
-    const y = SPIRAL_OFFSET_Y * i
+    let y = SPIRAL_OFFSET_Y * i + spiralY
+    y %= SPIRAL_OFFSET_Y * SPIRAL_SPLIT * SPIRAL_LOOP
+    if (y < 0) y += SPIRAL_OFFSET_Y * SPIRAL_SPLIT * SPIRAL_LOOP
 
     position.set(x, y, z)
   }
 
   calcSpiralPositionAndRotation(delta: number) {
-    this.spiralRot += delta / 1000 * AUTO_GLOBAL_ROT_SPEED
+    if (this.pointerState.down) {
+      this.spiralVelocity.rot = this.pointerState.deltaPos.x * .008
+      this.spiralVelocity.y = this.pointerState.deltaPos.y * .008 * -1
+
+      this.spiralRot += this.spiralVelocity.rot
+      this.spiralYByScroll += this.spiralVelocity.y
+    } else {
+      this.spiralRot += delta / 1000 * AUTO_GLOBAL_ROT_SPEED
+      this.spiralRot += this.spiralVelocity.rot
+      this.spiralYByScroll += this.spiralVelocity.y
+
+      this.spiralVelocity.rot *= .95
+      this.spiralVelocity.y *= .95
+    }
   }
 
   exec(delta: number) {
+    const rotRate = this.spiralRot / (Math.PI * 2)
+    const spiralYByRot = rotRate * SPIRAL_SPLIT * SPIRAL_OFFSET_Y
+    const spiralY = this.spiralYByScroll + spiralYByRot
+
     this.calcSpiralPositionAndRotation(delta)
 
     this.items.forEach((v, i) => {
-      this.calcItemPosition(i, this.spiralRot, v.object.position)
+      this.calcItemPosition(i, this.spiralRot, spiralY, v.object.position)
       if (v.isPlane) v.ajustPlaneShape(this.spiralRot)
       else v.rotate()
     })
+
+    this.pointerState.update()
   }
 }
 
